ccd2a36022c3762ba1ce9b56da1c51339f0c4c4e
johnstond <pipodavid@gmail.com>
2021-04-12 21:47:10 -0300

    Intento Nro 1

---
+++ Fire-init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB_HbLBaCT9iYNbamlOfPwOe3t43XByaa0",
+    authDomain: "nurse-control.firebaseapp.com",
+    projectId: "nurse-control",
+    storageBucket: "nurse-control.appspot.com",
+    messagingSenderId: "241593988388",
+    appId: "1:241593988388:web:e75c422b3dc783f515efb1",
+    measurementId: "G-PQQ1F468EF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
--- NC.js
+++ NC.js
@@ -1,10 +1,4 @@
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-    apiKey: "AIzaSyB_HbLBaCT9iYNbamlOfPwOe3t43XByaa0",
-    authDomain: "nurse-control.firebaseapp.com",
-    projectId: "nurse-control",
-  });
-  
+
 var db = firebase.firestore();
 
 db.collection("users").add({
