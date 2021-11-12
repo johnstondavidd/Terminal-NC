e14e7d0a4d6f9e8c17f88a542b92d35cb6e88e42
johnstond <pipodavid@gmail.com>
2021-11-12 19:45:31 -0300

    First commint with new windows

--- Register-sign.js
+++ Register-sign.js
@@ -1,134 +1,6 @@
 var db = firebase.firestore();
 
-function Register() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then(function(){
-    
-    Verify()
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-    // ..
-  });
-}
-
-function Sign() {
-  var email2 = document.getElementById('email2').value;
-  var password2 = document.getElementById('password2').value;
-
-  firebase.auth().signInWithEmailAndPassword(email2, password2)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-  });
-}
-
-function Observer() {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      console.log('Active user exists')
-      Appear(user);
-      //User signed in
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      console.log('------------------------')
-      console.log('Email verify:')
-      console.log(user.emailVerified)
-      console.log('------------------------')
-      var photoURL = user.photoURL;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-    } else {
-      console.log('Active user doesnt exists')
-      // User is signed out
-      // ...
-    }
-  });
-}
-
-Observer();
-
-function Appear(user) {
-  var user = user;
-  var content = document.getElementById('content');
-  if (user.emailVerified) {
-    
-    content.innerHTML= `
-    <h3 class="text-center">Add Patient</h3>
-    <div class="container">
-      <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand"></a>
-          <div class="form-inline">
-            <input id="DNI" type="text" placeholder="DNI" class="form-control mr-sm-2">
-            <input id="name" type="text" placeholder="Complete Name" class="form-control mr-sm-2">
-            <input id="age" type="text" placeholder="Age" class="form-control mr-sm-2">
-            <input id="room" type="text" placeholder="Room and bed" class="form-control mr-sm-2">
-            <input id="cause" type="text" placeholder="Cause" class="form-control mr-sm-2">
-            <input id="data" type="text" placeholder="Relevant Data" class="form-control mr-sm-2">
-            <button class="btn btn-outline-success my-2 my-sm-0" id="mybutton" onclick="SavePatient()">Add Patient</button>
-          </div>
-      </nav>
-    </div>
-    
-    <h3 class="text-center">First Search Patient to add History</h3>
-    <div class="container">
-      <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand"></a>
-          <div class="form-inline">
-            <input id="search" type="text" placeholder="DNI" class="form-control mr-sm-2">
-            <button class="btn btn-outline-success my-2 my-sm-0" onclick="PatientsSearch()">Search</button>
-          </div>
-      </nav>
-    </div>
-    
-   
-    <div class="mb-3">
-        <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">DNI</th>
-            <th scope="col">Name</th>
-            <th scope="col">Age</th>
-            <th scope="col">Room and bed</th>
-            <th scope="col">Cause</th>
-            <th scope="col">Relevant Data</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-            <th scope="col">Comments</th>
-          </tr>
-        </thead>
-        <tbody id="table">
-        </tbody>
-      </table>
-      </div>
-
-    
-
-    <button class="btn btn-danger position-absolute bottom-0 start-50 translate-middle-x" onclick="Close()">Logout</button>
-    
-    `
-
-  }
-  
-}
 
 function SavePatient() {
 
@@ -295,21 +167,9 @@ function Close() {
   .catch(function (error) {
     console.log(error)
   })
-  
+  location.href="file:///E:/Facultad/Proyecto%20de%20sist%20digitales/Proyecto%20FINAL/NC-Terminal/UserAccess.html"
 }
 
-function Verify() {
-  var user = firebase.auth().currentUser;
-user.sendEmailVerification()
-.then(function() {
-  console.log('Sending email...')
-  // Email sent.
-})
-.catch(function(error) {
-  console.log(error)
-  // An error happened.
-});
-}
 
 
 
---
+++ UserAccess.js
@@ -0,0 +1,98 @@
+var db = firebase.firestore();
+
+function Register() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(function(){
+    
+    Verify()
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ..
+  });
+}
+
+function Sign() {
+  var email2 = document.getElementById('email2').value;
+  var password2 = document.getElementById('password2').value;
+
+  firebase.auth().signInWithEmailAndPassword(email2, password2)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
+  Observer();
+}
+
+function Observer() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log('Active user exists')
+      Appear(user);
+      //User signed in
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      console.log('------------------------')
+      console.log('Email verify:')
+      console.log(user.emailVerified)
+      console.log('------------------------')
+      var photoURL = user.photoURL;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      console.log('Active user doesnt exists')
+      // User is signed out
+      // ...
+    }
+  });
+}
+
+//Observer();
+
+function Appear(user) {
+  var user = user;
+  var content = document.getElementById('content');
+  if (user.emailVerified) {
+      location.href="file:///E:/Facultad/Proyecto%20de%20sist%20digitales/Proyecto%20FINAL/NC-Terminal/index.html";
+   // var win = window.open('file:///E:/Facultad/Proyecto%20de%20sist%20digitales/Proyecto%20FINAL/NC-Terminal/index.html', '_blank');
+    
+  if (win) {
+    //Browser has allowed it to be opened
+    win.focus();
+  } else {
+    //Browser has blocked it
+    alert('Please allow popups for this website');
+    alert('Please Verify your email')
+}
+
+  }
+  
+}
+
+function Verify() {
+    var user = firebase.auth().currentUser;
+  user.sendEmailVerification()
+  .then(function() {
+    console.log('Sending email...')
+    // Email sent.
+  })
+  .catch(function(error) {
+    console.log(error)
+    // An error happened.
+  });
+  }
